72f10304722640c5ad13dd575e316040a63b0089
Delith <dmitry.kolesnykov@gmail.com>
2021-10-18 06:36:53 +0300

    remove env

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/kolesod/Projects/Proxet-Assignment/env/lib"
-XML2_LIBS="-lxml2 -L/home/kolesod/Projects/Proxet-Assignment/env/lib -lz -L/home/kolesod/Projects/Proxet-Assignment/env/lib -llzma -lpthread -L/home/kolesod/Projects/Proxet-Assignment/env/lib -L/home/kolesod/Projects/Proxet-Assignment/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/kolesod/Projects/Proxet-Assignment/env/include/libxml2 -I/home/kolesod/Projects/Proxet-Assignment/env/include"
-MODULE_VERSION="xml2-2.9.12"
-
